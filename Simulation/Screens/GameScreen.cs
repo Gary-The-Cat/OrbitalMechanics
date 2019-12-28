7f350e2a2f08e031f432576bbc266886bc4d5ac9
GaryTheCat <lukeberry9919@gmail.com>
2019-12-28 17:47:26 +1030

    Initial simulation commit

---
+++ Simulation/Screens/GameScreen.cs
@@ -0,0 +1,76 @@
+﻿using CarSimulation;
+using CarSimulation.Screens;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System.Collections.Generic;
+
+namespace Arkanoid_SFML.Screens
+{
+    public class GameScreen : Screen
+    {
+        internal RenderTexture texture;
+
+        internal int frame;
+
+        readonly Vector2f TitleBarSize = new Vector2f(12, 57);
+
+        List<Drawable> entities;
+
+        public Vertex[] background;
+
+        public GameScreen(
+            RenderWindow window,
+            FloatRect configuration)
+            : base(window, configuration)
+        {
+            frame = 0;
+            texture = new RenderTexture(Configuration.Width, Configuration.Height);
+
+            entities = new List<Drawable>();
+        }
+
+        /// <summary>
+        /// Update - Here we add all our logic for updating components in this screen. 
+        /// This includes checking for user input, updating the position of moving objects and more!
+        /// </summary>
+        /// <param name="deltaT">The amount of time that has passed since the last frame was drawn.</param>
+        public override void Update(float deltaT)
+        {
+            
+        }
+
+        /// <summary>
+        /// Draw - Here we don't update any of the components, only draw them in their current state to the screen.
+        /// </summary>
+        /// <param name="deltaT">The amount of time that has passed since the last frame was drawn.</param>
+        public override void Draw(float deltaT)
+        {
+            window.Clear();
+            texture.Clear();
+
+            texture.Draw(background, 0, 4, PrimitiveType.Quads);
+
+            foreach (var entity in entities)
+            {
+                texture.Draw(entity);
+            }
+
+            frame++;
+        }
+
+        public Vector2f GetMousePosition()
+        {
+            var position = Mouse.GetPosition();
+
+            var adjustedPosition = position - window.Position;
+
+            return new Vector2f(adjustedPosition.X - TitleBarSize.X, adjustedPosition.Y - TitleBarSize.Y);
+        }
+
+        public void AddVisual(Drawable visual)
+        {
+            entities.Add(visual);
+        }
+    }
+}
---
+++ Simulation/Screens/MainGameScreen.cs
@@ -0,0 +1,40 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace Arkanoid_SFML.Screens
+{
+    public class MainGameScreen : GameScreen
+    {
+        public MainGameScreen(RenderWindow window, FloatRect configuration) : base(window, configuration)
+        {
+            window.MouseButtonPressed += MouseButtonPressed;
+            window.MouseButtonPressed += MouseButtonReleased;
+        }
+
+        public override void Update(float deltaT)
+        {
+            base.Update(deltaT);
+        }
+
+        public override void Draw(float deltaT)
+        {
+            base.Draw(deltaT);
+
+            texture.Display();
+
+            var sprite = new Sprite(texture.Texture);
+
+            window.Draw(sprite);
+        }
+
+        private void MouseButtonPressed(object sender, MouseButtonEventArgs e)
+        {
+            var mousePosition = GetMousePosition();
+        }
+
+        private void MouseButtonReleased(object sender, MouseButtonEventArgs e)
+        {
+            var mousePosition = GetMousePosition();
+        }
+    }
+}
